07a808facdf30ee7995c81de88dc82781053895c
nabeman <tkmcoins20@gmail.com>
2023-03-01 19:09:07 +0900

    firstcommit

---
+++ index.js
@@ -0,0 +1,58 @@
+// import  express, { urlencoded } from 'express';
+const express = require('express');
+const urlencoded = require('express')
+const { default: axios } = require('axios');
+
+const APIKEY = "";
+const app = express();
+const PORT = 3000;
+
+const { Configuration, OpenAIApi} = require('openai');
+const configuration = new Configuration({
+    apiKey: APIKEY,
+})
+const openai = new OpenAIApi(configuration);
+
+//画像のurlを返す
+async function RequestImage(words){
+    let response = await openai.createImage({
+        prompt: words,
+        n: 1,
+        size: "1024x1024"
+    })
+    let image_url = response.data.data[0].url;
+    return image_url
+}
+
+app.use(express.json());
+app.use(express.static('src'));
+app.use(urlencoded({ extended: true }));
+
+app.get('/', async (_req, res) => {
+    return res.status(200).sendFile(__dirname + '/src/index.html');
+});
+
+app.post('/', async (req, res) => {
+    console.log("受け取った");
+    console.log(req.body['word']);
+    words = req.body['word'];
+    let url = await RequestImage(words); 
+    console.log("url取得完了");
+    let base;
+    axios.get(url).then((response) => {
+        base = response.data;
+    })
+    res.send(base);
+})
+
+try{
+    app.listen(PORT, () => {
+        console.log(`dev surver running at: http://localhost:${PORT}/`)
+    })
+}catch{
+    if(e instanceof Error){
+        console.error(e.message);
+    }
+}
+
+
---
+++ src/front.js
@@ -0,0 +1,42 @@
+const { createApp } = Vue;
+// const { axios } = Axios;
+const MAXLEN = 3;
+
+const app = createApp({
+    data(){
+        return{
+            word_list: ["tomato", "internet", "urban", "design", "game", "glass", "money", "book"],
+            select_list: []
+        }
+    },
+    methods: {
+        input_answer(word){
+            if(this.select_list.length >= MAXLEN){
+                this.select_list.push(word);
+                this.select_list.shift();
+            }
+            else{
+                this.select_list.push(word);
+            }
+            return console.log(word);
+        },
+        makeimg(){
+            let words = `${this.select_list[0]} ${this.select_list[1]} ${this.select_list[2]}`
+            let post = axios.post("http://localhost:3000/", { word: words }).then((response) => {
+                console.log("postで送信");
+                console.log(response.data);
+                this.catchimg(response.data);
+            }).catch((err) =>{
+                console.log("エラー");           
+                console.log(err);    
+            });
+        },
+        catchimg(img){
+            //imgはbase64形式
+            document.getElementById("img").src = img;
+            console.log(test);
+        },
+    }
+})
+
+app.mount("#main");
